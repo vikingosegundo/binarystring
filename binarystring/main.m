4f6ffaad8b8dc7b3d6c0e28b541e0e67a8d830ef
Manuel Meyer <manuel@vikingosegundo.de>
2013-02-19 10:08:19 +0100

    initial checkin

---
+++ binarystring/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  binarystring
+//
+//  Created by Manuel Meyer on 18.02.13.
+//  Copyright (c) 2013 yourcompany. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        NSUInteger length = 8;
+        NSUInteger n = pow(2, length);
+        for (int i = 0; i < n; ++i) {
+            NSString *repr = @"" ;
+            for (int j = 0; j < length; ++j) {
+                if([repr length] % 5 == 0)
+                    repr = [@" " stringByAppendingString:repr];
+                int x = (i >> j) & 1;
+                repr = [[NSString stringWithFormat:@"%u",x] stringByAppendingString:repr];
+            }
+            NSLog(@"%@", repr);
+        }
+    }
+    return 0;
+}
+
+
